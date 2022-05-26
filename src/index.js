fbaa30e3429b0ddcafbab9bada798d02e01e9246
johnnytan690 <94107329+johnnytan690@users.noreply.github.com>
2022-05-26 11:02:12 +0800

    Name and lucky number

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name = "Johnny";
+const first = "Johnny";
+const last = "Tan";
 var num = Math.floor(Math.random() * 10);
 
 ReactDOM.render(
   <div>
-    <h1>Hello {name}!</h1>
+    <h1>
+      Hello {first} {last}!
+    </h1>
     <p>Your lucky number is {num}. </p>
   </div>,
   document.getElementById("root")
